38beaf782270d18f6f129fcc011138c75082e02c
Jannik1020 <jannik.thiele.official@gmail.com>
2020-04-24 21:28:38 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import * as ReactRedux from "react-redux";
 import logo from './logo.svg';
 import './App.css';
+import Presentational from "./components/Presentational";
+import configureStore from "./store";
+import {changeQuoteAction} from "./actions/actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
+
+function mapStateToProps(state){
+  return{
+    currentQuote: state.randomQuoteReducer.currentQuote,
+    currentAuth: state.randomQuoteReducer.currentAuth
+  };
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    newQuote: () => {
+      dispatch(changeQuoteAction())
+    }
+  };
+}
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+class App extends React.Component{
+  render(){
+    return (
+      <Provider store={configureStore()}>
+        <Container />
+      </Provider>
+    );
+  }
+}
+
 
 export default App;
